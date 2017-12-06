c721d117c15322098c91425d0f75b4badb7dcd1c
Andrew Throener <athroener@mystartupsolutions.com>
2017-12-06 16:43:57 -0600

    Initial

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+let script = document.currentScript || document.getElementById("paypal-sfcc-client");
+console.log(script);
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+/*
+ * We've enabled ExtractTextPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/extract-text-webpack-plugin
+ *
+ */
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.ts',
+	devtool: 'inline-source-map',
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+
+				use: ExtractTextPlugin.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						}
+					],
+					fallback: 'style-loader'
+				})
+			}
+		]
+	},
+
+	plugins: [new UglifyJSPlugin(), new ExtractTextPlugin('style.css')]
+};
