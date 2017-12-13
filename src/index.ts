25903409fb2cff688e66305a404074bbc3cd4826
Andrew Throener <athroener@mystartupsolutions.com>
2017-12-12 21:06:11 -0600

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { getCurrentScript } from "./util";
 
 const script: any = getCurrentScript();
-console.info("script is", script["data-mini-cart"]);
+// console.info("script is", script["data-mini-cart"]);
 if (script["data-mini-cart"] !== null) {
     console.info("Importing MiniCart");
     import(/* webpackChunkName: "minicart" */ "./minicart")
