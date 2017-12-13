ea4d69da7c9c4d72b2fe39e3d8e057857f5ca2a3
Andrew Throener <athroener@mystartupsolutions.com>
2017-12-12 20:30:30 -0600

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { getCurrentScript } from "./util";
 
 const script: any = getCurrentScript();
-
+console.info("script is", script);
 if (script["data-mini-cart"]) {
     import(/* webpackChunkName: "minicart" */ "./minicart")
         .then(mcart => {
