d476c14d1ad40e2e3acff2f26039273fd87a70d0
Andrew Throener <athroener@mystartupsolutions.com>
2017-12-12 22:42:11 -0600

    Update

--- dist/src/shipping/index.d.ts
+++ dist/src/shipping/index.d.ts
@@ -2,7 +2,10 @@ export default class Shipping {
     static page: string;
     static form: {
         firstName: string;
+        lastName: string;
     };
     private paypalData;
     constructor();
+    private populateFields();
+    map(dwid: string): any;
 }
--- src/shipping/index.ts
+++ src/shipping/index.ts
@@ -5,18 +5,34 @@ export default class Shipping {
 
     static page = "shipping";
     static form = {
-        firstName: "dwfrm_singleshipping_shippingAddress_addressFields_firstName"
+        firstName: "dwfrm_singleshipping_shippingAddress_addressFields_firstName",
+        lastName: "dwfrm_singleshipping_shippingAddress_addressFields_lastName"
     };
 
     private paypalData: any;
 
     constructor() {
         this.paypalData = cookie.get("paypal-payment");
-        const firstname = document.getElementById(Shipping.form.firstName);
-        if (firstname) {
-            (firstname as HTMLInputElement).value = this.paypalData.payer.payer_info.first_name;
-        }
+        
+        this.populateFields();
 
+    }
+
+    private populateFields() {
+        for (const id in Shipping.form) {
+            const ele = document.getElementById(id);
+            if (ele) {
+                (ele as HTMLInputElement).value = this.map(id);
+            } 
+        }
+    }
+
+    map(dwid: string) {
+        if ( dwid === Shipping.form.firstName) {
+            return this.paypalData.payer.payer_info.first_name;
+        } else if (dwid === Shipping.form.lastName) {
+            return this.paypalData.payer.payer_info.last_name;
+        }
     }
 
 }
