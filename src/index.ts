16bfc06c79b204c4b6f76095b5b2a486d5565e58
Andrew Throener <athroener@mystartupsolutions.com>
2017-12-12 20:28:28 -0600

    Test

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const script: any = getCurrentScript();
 if (script["data-mini-cart"]) {
     import(/* webpackChunkName: "minicart" */ "./minicart")
         .then(mcart => {
-            
+            console.info("minicart: Imported");
         })
         .catch(error => "An error occurred while loading the minicart component");
 }
